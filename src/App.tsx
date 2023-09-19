3ba43ef5bd69cdd597739c5519587478602d1e00
GYauck <yauckgaston@gmail.com>
2023-09-19 03:01:37 -0300

    started favourites page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Navbar from "./components/Navbar/Navbar";
+import Favourites from "./pages/Favourites/Favourites";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/favourites" element={<Favourites/>}/>
       </Routes>
     </>
   );
